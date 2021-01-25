4abbd7bde06adfd42dc7fa2f07298675a698d7ac
flyaway12345 <evanhaggard123@gmail.com>
2021-01-25 14:43:25 -0500

    Added Buttons And Assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Stress-Button';
+
+  playPop(){
+    let audio = new Audio();
+    audio.src = "src/assets/pop.mp3";
+    audio.load();
+    audio.play();
+  }
 }
